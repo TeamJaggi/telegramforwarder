6bba90ae9906e9f38bf2417aff064d6df644638b
TeamJaggi <nftjaggi@gmail.com>
2025-09-02 16:46:39 +0530

    Delete session.py

--- session.py
+++
@@ -1,9 +0,0 @@
-from telethon.sync import TelegramClient
-from telethon.sessions import StringSession
-
-API_ID = 29657994  # yha session id dalna hai 
-API_HASH = "85f461c4f637911d79c65da1fc2bdd77"  # api hash
-SESSION_NAME = "user_session"  # session ka name 
-
-with TelegramClient(SESSION_NAME, API_ID, API_HASH) as client:
-    print("session ban gya ", SESSION_NAME + ".session")
